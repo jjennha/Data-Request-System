f62db21fe2cf1fb6d6ba41c67906fa8dba3e582b
Cody Lakin <cody.lakin@buildertrend.com>
2018-07-25 09:18:12 -0500

    Completed Request page added collapsable panels, knockout databinds. Created app.js containing the view model.

--- DataRequestSystem/DataRequestSystem/App_Start/RouteConfig.cs
+++ DataRequestSystem/DataRequestSystem/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace DataRequestSystem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "RequestSubmission", id = UrlParameter.Optional }
             );
         }
     }
